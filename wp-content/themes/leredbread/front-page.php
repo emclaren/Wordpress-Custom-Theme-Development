6aedc7b7baff70a7eab7ff8cc03b7f216f7cdcbf
emclaren <elgin_mclaren@hotmail.com>
2015-10-30 11:46:43 -0700

    initiate github repository

---
+++ wp-content/themes/leredbread/front-page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<?php get_template_directory_uri().'https://upload.wikimedia.org/wikipedia/commons/c/c7/Puppy_on_Halong_Bay.jpg' ?>
+<p>test </p>
+
+</div>
